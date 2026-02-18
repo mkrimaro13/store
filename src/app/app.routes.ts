652df18d49257fc4a671e13e42db585d46e7ad94
Marco Osorio <marco.osorio86@hotmail.com>
2026-02-17 21:20:11 -0500

    Cambios a nivel base, se agregan rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { List } from './domains/products/pages/list/list';
+import { About } from './domains/info/pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: List,
+  },
+  {
+    path:'about',
+    component: About
+  }
+];
