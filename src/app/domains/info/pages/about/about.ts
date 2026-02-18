3424eb7ac817b5abc5c4d5c22373becc26f505b3
Marco Osorio <marco.osorio86@hotmail.com>
2026-02-17 21:20:47 -0500

    about simple

---
+++ src/app/domains/info/pages/about/about.ts
@@ -0,0 +1,23 @@
+import { Component, signal, WritableSignal } from '@angular/core';
+import { Counter } from '../../../shared/components/counter/counter';
+
+@Component({
+  selector: 'app-about',
+  imports: [Counter],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {
+  duration: WritableSignal<number> = signal(15);
+  message: WritableSignal<string> = signal('Hello!');
+
+  changeDuration(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.duration.set(Number(input.value));
+  }
+
+  changeMessage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.message.set(input.value);
+  }
+}
