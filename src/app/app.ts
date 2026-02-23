3e4374d719cb54224f6674ad2ade18c9290a83da
Marco Osorio <marco.osorio86@hotmail.com>
2026-02-23 08:28:17 -0500

    Se cambia el color del fondo, el color de la letra y la sombra del header

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet />',
+  template: '<router-outlet class="bg-[#222831]"/>',
 })
 export class App {
   protected readonly title = signal('store');
